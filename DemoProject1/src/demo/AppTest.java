e44bb45c377ce268086ae0ef95ac728b93a91447
sudip <sudip@192.168.0.3>
2018-11-22 18:45:50 -0500

    My Second Java Commit

---
+++ DemoProject1/src/demo/AppTest.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class AppTest {
+
+	public static void main(String[] args) {
+		SalesData sdata = new SalesData();
+		
+		displayGreeting();
+		sdata.display();
+		
+		
+	}
+	private static void displayGreeting() {
+		System.out.println("Hello!, Happy Sales People");
+		System.out.println("This App shows sales data");
+		
+	}
+
+}
---
+++ DemoProject1/src/demo/SalesData.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class SalesData {
+	
+	int[] data = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("TODO: Implement Data Display");
+	}
+
+}
